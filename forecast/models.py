f44a10243fe2fae92788833e5a015de31c5fabb4
bhargavtentu <bhargavtentu26@gmail.com>
2025-06-20 14:07:20 +0530

    DB

--- forecast/models.py
+++ forecast/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    name = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
